cbe16f00b84857238607d83ab821e7bf74dceb8b
Sristhi lamichhane <srishtilamichhane83@gmail.com>
2024-05-15 10:53:13 +0545

    feat: :sparkles: Sidebar.tsx work in progress

---
+++ app/(dashboard)/_components/Sidebar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Logo from "./Logo";
+
+const Sidebar = () => {
+  return (
+    <div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
+      <div className="p-6">
+        <Logo/>
+
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
