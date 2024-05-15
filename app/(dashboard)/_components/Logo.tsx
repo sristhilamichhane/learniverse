a0a66589c9179f51c72c4f0d28909555392385f9
Sristhi lamichhane <srishtilamichhane83@gmail.com>
2024-05-15 10:52:47 +0545

    feat: :sparkles: For logo inside sidebar

---
+++ app/(dashboard)/_components/Logo.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Image from "next/image";
+
+const Logo = () => {
+  return <Image height={130} width={130} alt="logo" src="/logo.png" />;
+};
+
+export default Logo;
