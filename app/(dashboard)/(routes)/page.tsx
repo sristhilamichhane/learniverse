0c952f2d428da72e292ee6f7205aece195de2817
Sristhi lamichhane <srishtilamichhane83@gmail.com>
2024-05-15 10:52:33 +0545

    feat: :sparkles: main page

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+}
