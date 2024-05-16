493919cf2e254bb711c97d0e072cee66bdb4f264
Sristhi lamichhane <srishtilamichhane83@gmail.com>
2024-05-16 15:13:51 +0545

    style: :zap: added some styling on Layout fro sidebar plus Navbar view on Dashboard layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,14 +1,21 @@
+import React from "react";
 import Sidebar from "./_components/Sidebar";
+import Navbar from "./_components/Navbar";
 
-const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function layout({ children }: Props) {
   return (
     <div className="h-full">
+      <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
+        <Navbar />
+      </div>
       <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
         <Sidebar />
       </div>
-      {children}
+      <main className="md:pl-56 pt-[80px] h-full"> {children}</main>
     </div>
   );
-};
-
-export default DashboardLayout;
+}
