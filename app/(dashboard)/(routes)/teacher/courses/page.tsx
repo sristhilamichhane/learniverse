f310463b55a8d4a944541095615e70fb1edbd95d
Sristhi lamichhane <srishtilamichhane83@gmail.com>
2024-06-04 17:37:53 +0545

    feat: :sparkles: data table added in course page of teacher

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,15 +1,32 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import React from "react";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "LMS | Course list",
+  description: "Leaning Management System |LMS",
+};
+const CoursesPage = async () => {
+  let { userId } = auth();
 
-type Props = {};
-
-export default function page({}: Props) {
+  if (!userId) {
+    return redirect("/");
+  }
+  const course = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
   return (
     <div className="p-6">
-      <Link href="/teacher/create">
-        <Button>New Course</Button>
-      </Link>
+      <DataTable columns={columns} data={course} />
     </div>
   );
-}
+};
+
+export default CoursesPage;
