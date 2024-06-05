104a10821016bb02f146cb2cf98c76ce67633c47
Sristhi lamichhane <srishtilamichhane83@gmail.com>
2024-06-05 08:46:46 +0545

    feat: :sparkles: search page

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,5 +1,10 @@
 import { db } from "@/lib/db";
 import Categories from "./_components/categories";
+import SearchInput from "@/components/search-input";
+import { getCourses } from "@/actions/get-courses";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import CoursesList from "@/components/courses-list";
 
 interface SearchPageProps {
   searchParams: {
@@ -7,16 +12,33 @@ interface SearchPageProps {
     categoryId: string;
   };
 }
-
 const SearchPage = async ({ searchParams }: SearchPageProps) => {
+  let { userId } = auth();
+  // let userId = "user_2c7WDRhRgaTXgF3G3JIaInZbQD4";
+
+  if (!userId) {
+    return redirect("/");
+  }
+
   const categories = await db.category.findMany({
     orderBy: { name: "asc" },
   });
 
+  const courses = await getCourses({
+    userId,
+    ...searchParams,
+  });
+
   return (
-    <div className="p-6 space-x-4">
-      <Categories items={categories} />
-    </div>
+    <>
+      <div className="px-6 pt-6 md:hidden md:mb-0 block">
+        <SearchInput />
+      </div>
+      <div className="p-6 space-x-4">
+        <Categories items={categories} />
+        <CoursesList items={courses} />
+      </div>
+    </>
   );
 };
 
