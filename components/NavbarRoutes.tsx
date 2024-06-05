01760cdde11aa7c08a0044cb7ff2e81d76ab1156
Sristhi lamichhane <srishtilamichhane83@gmail.com>
2024-06-05 19:22:47 +0545

    feat: :hammer: small modification to show exit button inside navbar after getting into course

--- components/NavbarRoutes.tsx
+++ components/NavbarRoutes.tsx
@@ -12,7 +12,7 @@ type Props = {};
 export default function NavbarRoutes({}: Props) {
   const pathname = usePathname();
   const isTeacherPage = pathname?.startsWith("/teacher");
-  const isPlayerPage = pathname?.includes("/chapter");
+  const isPlayerPage = pathname?.includes("/courses");
 const isSearchPage = pathname === "/search";
 
   return (
