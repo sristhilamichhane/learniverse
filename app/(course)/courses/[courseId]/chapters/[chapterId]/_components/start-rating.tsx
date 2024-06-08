8a60c8cf6506472592f280dff9a5912b8d5879d9
Sristhi lamichhane <srishtilamichhane83@gmail.com>
2024-06-08 09:47:36 +0545

    feat: :sparkles: for testing purpose only

---
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/start-rating.tsx
@@ -0,0 +1,48 @@
+"use client";
+import React, { useState, FC } from "react";
+
+interface StarRatingProps {
+  onChange?: (value: number) => void;
+  value?: number;
+  starSize?: string;
+}
+
+const StarRating: FC<StarRatingProps> = ({
+  onChange,
+  value,
+  starSize = "text-4xl",
+}) => {
+  const [rating, setRating] = useState<number>(value || 0);
+
+  const handleStarClick = (
+    event: React.MouseEvent<HTMLButtonElement>,
+    value: number
+  ) => {
+    event.preventDefault(); // Prevent default form submission
+    setRating(value);
+    if (onChange) {
+      onChange(value);
+    }
+  };
+
+  const emptyStarIcon = "☆";
+  const starIcon = "★";
+
+  return (
+    <div className="flex items-center gap-x-2">
+      {[1, 2, 3, 4, 5].map((star) => (
+        <button
+          key={star}
+          onClick={(event) => handleStarClick(event, star)}
+          className={`text-yellow-500 ${
+            star <= rating ? "fill" : ""
+          } ${starSize}`}
+        >
+          {star <= rating ? starIcon : emptyStarIcon}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default StarRating;
